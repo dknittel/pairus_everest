c3cb48bd8b8f5e8c3caa23a0e42d329d70802b4e
James Bomotti <zilliahix@gmail.com>
2015-09-18 11:48:15 -0700

    Add devise and user MVC

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Devise/Heroku
+  ###############################################
+  #### Change to actual host name once deployed
+  ###############################################
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
