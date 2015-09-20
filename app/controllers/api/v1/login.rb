b4698caec7d8f305ee4f0b57d5d9fa21aca1a9a2
Tarik Bennett <tarik.bennett@gmail.com>
2015-09-20 12:36:45 -0700

    Update api controller V1

--- app/controllers/api/v1/login.rb
+++ app/controllers/api/v1/login.rb
@@ -56,3 +56,18 @@ private topic_params
 end
 
 end
+
+class AvailabilitiestController < ApplicationController
+
+  def create
+    group = Group.find(params[:group_id])
+    @topic = group.topics.create(params[:title])
+
+    # this should send back all topics with newly created topic
+  end
+
+private topic_params
+  params.require(:group).permit(:title)
+end
+
+end
