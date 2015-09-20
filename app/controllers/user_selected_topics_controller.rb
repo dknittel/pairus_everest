ab137075c340f02bdeea9a1690c78f970b3c52a7
[01;31m[K"name"[m[K: "Erick Gist <[01;31m[K"email"[m[K: null>
2015-09-20 11:13:32 -0700

    morechanges to Update Views

--- app/controllers/user_selected_topics_controller.rb
+++ app/controllers/user_selected_topics_controller.rb
@@ -1,8 +1,9 @@
 class UserSelectedTopicsController < ApplicationController
   def create
-    p params
     user = User.find(current_user.id)
-    user.user_selected_topics << params[]
-    we need to create a user_selected topic with the user id
+    topics = params[:topic].keys
+    topics.each do |topic|
+    user.user_selected_topics << topic.id
+    end
   end
 end
