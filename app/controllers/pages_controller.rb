47a8905cefdc3dda0f1bdbad75290c634c9a33ac
[01;31m[K"name"[m[K: "Erick Gist <[01;31m[K"email"[m[K: null>
2015-09-19 23:31:00 -0700

    Add api controller files + home view files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+    if session[:user_id]
+      @groups = []
+      user = User.find(current_user.id)
+      user_groups = user.user_groups
+      user_groups.each do |user_group|
+        @groups << Group.find(user_group.group_id)
+      end
+      @groups = user.groups
+    end
   end
 end
