6edf1f7a8b112b123d3d6f5686e65fa68a354c25
Daniel Knittel <daknittel11@gmail.com>
2015-09-21 15:13:06 -0700

    fix bug with hour in potentialpair controller#create

--- app/helpers/availabilities_helper.rb
+++ app/helpers/availabilities_helper.rb
@@ -2,9 +2,6 @@ module AvailabilitiesHelper
 	def user_helper(avail)
 		ust = UserSelectedTopic.find(avail.user_selected_topic_id)
 		user = User.find(ust.user_id)
-		# p 'x' * 100
-		# p user
-		# return user
 	end
 
 	def hour_helper(avail)
