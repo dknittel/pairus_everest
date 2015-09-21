9a45357d7bd001c5b295db8c47fe4ec6557c7256
[01;31m[K"name"[m[K: "Erick Gist <[01;31m[K"email"[m[K: null>
2015-09-21 13:34:25 -0700

    Add logic to controller logic

--- app/controllers/potential_pairs_controller.rb
+++ app/controllers/potential_pairs_controller.rb
@@ -2,6 +2,10 @@ class PotentialPairsController < ApplicationController
   def index
     group = Group.find(params[:group_id])
     redirect_to group_path(group)
+    # this is where the Pairing list should be displayed from
+    # if PotentialPair.user1_accepted == "true" || PotentialPair.user2_accepted == "true"
+    # display the PotentialPair info
+    # end
   end
 
   def show
@@ -11,6 +15,9 @@ class PotentialPairsController < ApplicationController
   end
 
   def create
+    # if PotentialPair exist with same topic, hour, and both users
+    # then do not create but update
+    # else if PotentialPair doesn't exist create one
     current_avail = Availability.find_by(user_selected_topic_id: UserSelectedTopic.find_by(user_id: current_user.id), hour_id: Hour.find_by(hr: params[:time][:hour]))
     # User.find()
     pair_user = User.find(params[:user_id])
