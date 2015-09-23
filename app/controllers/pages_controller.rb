2df66ae4c00eec4ba1f1b64b98b20c9721c010ee
Daniel Knittel <daknittel11@gmail.com>
2015-09-22 18:05:47 -0700

    minor styling for topics page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
   def home
     @group_id = params[:group]
   	if user_signed_in?
-      if session[:group_id]
+      p '*' * 100
+      p session[:group_id]
+      if session[:group_id] != ""
         group_id = session[:group_id]
         group = Group.find(group_id)
         group.users << current_user
