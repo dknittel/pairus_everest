54431f2a21dc34cd384d6fc9bbe9a30eaf0bb36e
Tarik B <tarik.bennett@gmail.com>
2015-09-24 09:56:43 -0700

    Update api controllers. Matches, hours

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-
   get 'user_selected_topics/create'
 
   get 'pages/home'
@@ -8,13 +7,7 @@ Rails.application.routes.draw do
   post 'user_selected_topics/create' => 'user_selected_topics#create'
 
   devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-  namespace :api do
-    namespace :v1 do
-      post 'login' => 'login#login'
-    end
-  end
+
   # You can have the root of your site routed with "root"
   root 'pages#home'
   namespace :api do
