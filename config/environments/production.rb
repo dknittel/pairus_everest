0ae271ebad19fe4c6f6a357cd7f6713ed92af5b8
Daniel Knittel <daknittel11@gmail.com>
2015-11-11 15:42:33 -0800

    change from url shortener from localhost to pairus-app.heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,5 @@ Rails.application.configure do
   ###############################################
   #### Change to actual host name once deployed
   ###############################################
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'pairus-app.herokuapp', port: 3000 }
 end
