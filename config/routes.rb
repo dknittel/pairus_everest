03bc8d4a54505cac2a6718dedd4c6feeb959e3e1
Daniel Knittel <daknittel11@gmail.com>
2015-11-11 14:05:57 -0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
 
   get 'user_selected_topics/create'
 
-  get 'pages/home'
+  get 'pages/home' => 'pages#home'
   get 'groups/:group_id/invites' => 'groups#invites'
   post 'user_selected_topics/create' => 'user_selected_topics#create'
 
   devise_for :users
 
   # You can have the root of your site routed with "root"
-  root 'pages/home'
+  root 'pages#home'
   namespace :api do
     namespace :v1 do
       resources :users do
